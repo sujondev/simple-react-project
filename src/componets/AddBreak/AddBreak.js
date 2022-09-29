e6d5988d8cc8ed30acc2e6135308a49a84e5cadf
sujondev <emd26521@gmail.com>
2022-09-29 21:48:15 +0600

    local storage set item

--- src/componets/AddBreak/AddBreak.js
+++ src/componets/AddBreak/AddBreak.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './AddBreak.css'
 
 const AddBreak = ({ setBreakTime }) => {
+
     return (
         <div className='btn-group'>
-            <button onClick={() => setBreakTime(20)}>20s</button>
+            <button onClick={() => {
+                setBreakTime(20)
+                localStorage.setItem("breakTime", JSON.stringify(20));
+                // console.log(typeof localStorage.getItem("breakTime"));
+            }}>20s</button>
             <button onClick={() => setBreakTime(30)}>30s</button>
             <button onClick={() => setBreakTime(40)}>40s</button>
             <button onClick={() => setBreakTime(50)}>50s</button>
